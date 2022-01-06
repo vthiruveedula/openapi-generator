44621b3dbda1c087b2bce91b3daed2e1e9e3ab26
William Cheng <wing328hk@gmail.com>
2022-01-06 21:32:24 +0800

    run /opt/circleci/.pyenv/plugins/pyenv-update

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -57,9 +57,9 @@ elif [ "$NODE_INDEX" = "3" ]; then
   #cd Python-3.8.9
   #./configure --enable-optimizations
   #sudo make altinstall
-  rm -Rf /opt/circleci/.pyenv
-  curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-  exec $SHELL
+  #rm -Rf /opt/circleci/.pyenv
+  #curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
+  /opt/circleci/.pyenv/plugins/pyenv-update
   pyenv --version
   pyenv install --list 
   pyenv install 3.6.3
